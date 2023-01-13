05e38e4a8253213c59295ec1f44de1706c3813f1
ElusiveJoe96 <104578457+ElusiveJoe96@users.noreply.github.com>
2023-01-13 17:31:46 +0300

    init comm

---
+++ src/main/java/ru/slukin/PtLab2Application.java
@@ -0,0 +1,13 @@
+package ru.slukin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtLab2Application.class, args);
+    }
+
+}
