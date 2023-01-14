355fa66f8a5122502d9435ef5ab7c58d121697a9
ElusiveJoe96 <104578457+ElusiveJoe96@users.noreply.github.com>
2023-01-14 16:13:03 +0300

    add validation

--- src/main/java/ru/slukin/controllers/PeopleController.java
+++ src/main/java/ru/slukin/controllers/PeopleController.java
@@ -1,8 +1,10 @@
 package ru.slukin.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.slukin.dao.PersonDAO;
 import ru.slukin.models.Person;
@@ -21,22 +23,26 @@ public class PeopleController {
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
-        return "index";
+        return "/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
-        return "show";
+        return "/show";
     }
 
     @GetMapping("/new")
     public String newPerson(@ModelAttribute("person") Person person) {
-        return "new";
+        return "/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -44,11 +50,15 @@ public class PeopleController {
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("person", personDAO.show(id));
-        return "edit";
+        return "/edit";
     }
 
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    @PostMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
